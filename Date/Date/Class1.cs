dfd0277076f9a5780d6b448b027d28583717f7f2
Henk Burgstra <henk.burgstra@gmail.com>
2016-04-17 23:42:02 +0200

    Begin DateExtensions class

---
+++ Date/Date/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Date.Net
+{
+    public struct Age
+    {
+        public int Years;
+        public int Months;
+    }
+
+    public static class DateExtensions
+    {
+        public static Age Age(this DateTime date)
+        {
+            var age = new Age();
+            return age;
+        }
+
+    }
+}
