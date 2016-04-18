17cb38877a87652d53cc1846ae3931591087fd81
Henk Burgstra <henk.burgstra@gmail.com>
2016-04-18 09:32:16 +0200

    Extension methods toegevoegd.

--- Date/Date/Date.Net.cs
+++ Date/Date/Date.Net.cs
@@ -13,9 +13,29 @@ namespace Date.Net
 
     public static class DateExtensions
     {
-        public static Age Age(this DateTime date)
+
+        public static DateTime AddWeeks(this DateTime date, int weeks)
         {
+            return date.AddDays(7 * weeks);
+        }
+        public static DateTime AddQuarters(this DateTime date, int quarters)
+        {
+            return date.AddMonths(3 * quarters);
+        }
+        public static Age Age(this DateTime date, DateTime referenceDate = default(DateTime))
+        {
+            if (referenceDate.Year == 1)
+            {
+                referenceDate = DateTime.Today;
+            }
+            var months = (referenceDate.Year - date.Year) * 12 + (referenceDate.Month - date.Month);
+            if (referenceDate.Day < date.Day)
+            {
+                months--;
+            }
             var age = new Age();
+            age.Years = months / 12;
+            age.Months = months % 12;
             return age;
         }
 
